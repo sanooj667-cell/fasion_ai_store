ca6b0a1ff64d57ee92c40b11954144188833492c
Sanooj <sanooj667@gmail.com>
2026-05-01 16:08:18 +0530

    creat product detailing api

--- products/views.py
+++ products/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .models import Product, Category
@@ -12,3 +12,17 @@ def product_list(request):
     serializer = ProductSerializer(product, many=True)
     return Response(serializer.data) 
 
+
+
+
+@api_view(['GET'])
+def product_detail(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    related_products = product.objects.filter(category=product.category).exclude(id=product.id)[:4]
+    serilizer = ProductSerializer(product)
+    related_serializer = ProductSerializer(related_products, many=True)
+
+    return Response({
+        'product': serilizer.data,
+        'related_products': related_serializer.data
+    })
